4e93d97a70295ca346fd5a4845c090f3fe8843ac
Dave Henton <dave@codeclimate.com>
2017-11-21 16:33:37 -0500

    Create example.js

---
+++ nest1/nest2/nest3/example.js
@@ -0,0 +1,184 @@
+
+function isPrime(n){
+  var divisor = 2;
+roger
+roger
+roger
+roger
+rogerroger
+roger
+roger
+roger
+fixme
+FIXME
+FIXME
+FIXME
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+rogerroger
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
+        
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+> isPrime(137);
+  = true
+> isPrime(237);
+  = false
+        
