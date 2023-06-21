be34a5af2babb6f73477cac1ca6b1283e0c86681
wharshad19 <111437752+wharshad19@users.noreply.github.com>
2023-06-21 23:22:50 +0530

    Update foo_bar.rb

--- foo_bar.rb
+++ foo_bar.rb
@@ -1,1 +1,30 @@
-# TODO: Add FooBar content
+require "securerandom"
+
+module DuplicationExample
+  def identical(message, header: true)
+    body = []
+    body << "-- HEADER --" if header
+    body << "-- MESSAGE --"
+    body << message
+    body << "-- FOOTER --"
+    message = body.join("\n")
+
+    if message.length > 100
+      message[0, 100]
+    else
+      message
+    end
+  end
+
+  def similar
+    hex = SecureRandom.hex(20)
+    hex.reverse
+    hex += "-suffix"
+
+    if hex.start_with?("z")
+      hex += "something-else"
+    end
+
+    hex
+  end
+end# TODO: Add FooBar content
