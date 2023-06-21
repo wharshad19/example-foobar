57434f985f2c06bbe160d43546272c36c13dfe38
wharshad19 <111437752+wharshad19@users.noreply.github.com>
2023-06-21 23:41:28 +0530

    Update foo_bar_spec.rb

--- spec/foo_bar_spec.rb
+++ spec/foo_bar_spec.rb
@@ -1,4 +1,28 @@
+# TODO: Add FooBar specs
 require "spec_helper"
 require_relative "../foo_bar"
 
-# TODO: Add FooBar specs
+describe "FooBar" do
+  it "returns correct list of items" do
+    result = FooBar.new.run(15)
+
+    expect(result.length).to eq(15)
+    expect(result).to eq([
+      1,
+      2,
+      "FOO",
+      4,
+      "BAR",
+      "FOO",
+      7,
+      8,
+      "FOO",
+      "BAR",
+      11,
+      "FOO",
+      13,
+      14,
+      "FOOBAR",
+    ])
+  end
+end
